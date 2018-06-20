7a072c35eb5b855cc9371832354a731c6f62939e
Alyssampratt <magoopratt@gmail.com>
2018-06-20 20:56:05 +0000

    Done.

---
+++ trips.rb
@@ -0,0 +1,9 @@
+dream_vacations={
+"sonali" => "carribbean",
+"evie" => "europe",
+"sam" => "barbados",
+"isabella" => "australia",
+"millie" => "paris"
+}
+puts dream_vacations.keys
+puts dream_vacations.values
