47bf8d07e13734c21dab3e5d4247fc1cbadffb11
annaliner <annaliner89@gmail.com>
2018-06-20 16:56:09 +0000

    Done.

---
+++ more_arrays.rb
@@ -0,0 +1,11 @@
+# farmer_tasks= ["Milk the cow", "Churn the butter", "Drive the tractor","Peel potatoes","Plant the corn"]
+# farmer_tasks.each do |key|
+#   puts "I'm so busy! I need to #{key}"
+# end
+
+
+annas_life = ["eat bok choy", "hang out with bok choy", "dance with bok choy", "sing bok choy to sleep"]
+annas_life.each do |bok|
+ puts "Oh crud!!!!!! I gotta go #{bok}????"
+ end
+
