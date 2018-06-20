4e3c02a0c282a3aabb5e27805e9ac5e6dcea8108
Alyssampratt <magoopratt@gmail.com>
2018-06-20 21:32:13 +0000

    Done.

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -1,13 +1,19 @@
+def create_olympics_hash
 summer_olympics= {
   :Sydney => 2000,
   :Athens => 2004,
   :Beijing => 2008,
   :London => 2012
 }
-summer_olympics [:Atlanta => 1996]
 
-year=summer_olympics.value
-place=summer_olympics.key
 
-puts "The #{year} summer olympics took place in #{place}"
+summer_olympics[:Atlanta] = 1996
+year=summer_olympics.keys
+place=summer_olympics.values
+summer_olympics.each do |year, place|
+puts "The #{year} summer olympics took place in #{place}" 
+end 
+end
+
+create_olympics_hash
 
