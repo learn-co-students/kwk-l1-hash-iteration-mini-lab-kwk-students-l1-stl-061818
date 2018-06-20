88a3ace473e2b7a355e4c84e18f7923a4a44d5f6
annaliner <annaliner89@gmail.com>
2018-06-20 18:53:18 +0000

    Done.

---
+++ celebrity_crush.rb
@@ -0,0 +1,43 @@
+# array inside an array 
+
+
+# #this is a hash
+# celebrity_crush= {
+#   "Lily" => "Matt Bomer",
+#   "Isabella"=> "Tom Holland",
+#   "Sam"=> "Michael B Jordan",
+#   "Bella"=> "Dallas Winston",
+#   "Evie"=> "My Self"
+# }
+
+# puts celebrity_crush.keys
+
+# bok_choy_coders=["Brianna", "Sonali", ["Caroline", "Kathyn", "Sam"] , "Amy", "Alice"]
+# puts bok_choy_coders[2][0]
+
+
+world= {
+  "Brynne"=>"Japan",
+  "Cassie"=>"New Zealand",
+  "Caroline"=>"Caroline Islands",
+  "Lily"=>"New Zealand",
+  "Karina"=>"France"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
