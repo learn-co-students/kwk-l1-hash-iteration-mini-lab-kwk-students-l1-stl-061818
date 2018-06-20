4cc30a4d0fb24d96791d695900017ab5c6813207
Alyssampratt <magoopratt@gmail.com>
2018-06-20 21:13:31 +0000

    Done.

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -1,10 +1,10 @@
 summer_olympics= {
-  :Atlanta => 1996
   :Sydney => 2000,
   :Athens => 2004,
   :Beijing => 2008,
   :London => 2012
 }
+summer_olympics [:Atlanta => 1996]
 
 year=summer_olympics.value
 place=summer_olympics.key
